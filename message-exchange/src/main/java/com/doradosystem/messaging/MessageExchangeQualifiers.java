27b93ad2dc003d962981a2c6b9d141f071bf680a
Carlos Devoto <cdevoto@maddogtechnology.com>
2016-08-19 07:15:22 -0400

    Factored out all named qualifiers into a MessageExchangeQualifiers class.

---
+++ message-exchange/src/main/java/com/doradosystem/messaging/MessageExchangeQualifiers.java
@@ -0,0 +1,9 @@
+package com.doradosystem.messaging;
+
+public class MessageExchangeQualifiers {
+  public static final String INPUT_EVENT_BUS = "input-channel-event-bus";
+  
+  
+  private MessageExchangeQualifiers() {}
+
+}
--- message-exchange/src/main/java/com/doradosystems/messaging/input/InputChannelModule.java
+++ message-exchange/src/main/java/com/doradosystems/messaging/input/InputChannelModule.java
@@ -1,11 +1,14 @@
 package com.doradosystems.messaging.input;
 
+import static com.doradosystem.messaging.MessageExchangeQualifiers.INPUT_EVENT_BUS;
+
 import javax.annotation.Nonnull;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.devoware.homonculus.setup.Environment;
 
+import com.doradosystem.messaging.MessageExchangeQualifiers;
 import com.google.common.eventbus.EventBus;
 
 import dagger.Module;
@@ -14,11 +17,13 @@ import dagger.Provides;
 @Module
 public class InputChannelModule {
 
-  @Provides @Named("input-channel-event-bus")
+  private static final String EVENT_BUS_NAME = "input-channel";
+
+  @Provides @Named(INPUT_EVENT_BUS)
   @Singleton
   @Nonnull
   public EventBus providesInputChannelEventBus() {
-    return new EventBus("input-channel");
+    return new EventBus(EVENT_BUS_NAME);
   }
   
   @Provides
